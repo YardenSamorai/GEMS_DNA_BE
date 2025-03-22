327bb7c649bf71f371392ce9b268693642c748bd
Yarden Samorai <yardensamorai6@gmail.com>
2025-03-22 14:12:30 +0200

    Change Critical env file.

--- import-csv.js
+++ import-csv.js
@@ -5,7 +5,7 @@ const csvParser = require("csv-parser");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: "postgresql://gems_owner:npg_mr64DAeRqOfd@ep-bitter-paper-a5t16ihi-pooler.us-east-2.aws.neon.tech/gems?sslmode=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // ✅ Required for Neon
   },
